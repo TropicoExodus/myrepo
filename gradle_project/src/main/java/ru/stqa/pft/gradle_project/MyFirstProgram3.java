b035d1ca6839e5f1472c287a1554614c713142ef
TropicoExodus <147339591+TropicoExodus@users.noreply.github.com>
2023-10-22 21:50:47 +0300

    Работа с переменными

--- gradle_project/src/main/java/ru/stqa/pft/gradle_project/MyFirstProgram3.java
+++ gradle_project/src/main/java/ru/stqa/pft/gradle_project/MyFirstProgram3.java
@@ -2,6 +2,23 @@ package ru.stqa.pft.gradle_project;
 
 public class MyFirstProgram3 {
     public static void main (String [] args) {
-        System.out.println("Hello, fcng world");
+        System.out.println("Hello, wonderful world");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1.0/2);
+
+        System.out.println("2" + "2");
+        System.out.println(2 + "2");
+
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+
+
+
+
     }
 }
