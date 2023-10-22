b35250b8802596668a1655b117d1b634c2378d26
TropicoExodus <147339591+TropicoExodus@users.noreply.github.com>
2023-10-22 22:07:59 +0300

    работа со значениями и переменными

--- gradle_project/src/main/java/ru/stqa/pft/gradle_project/MyFirstProgram1.java
+++ gradle_project/src/main/java/ru/stqa/pft/gradle_project/MyFirstProgram1.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.gradle_project;
 public class MyFirstProgram1 {
 	
 	public static void main (String [] args) {
-		System.out.println("Hello, world");	
+		String somebody = "world";
+		System.out.println("Hello, " + somebody + "!");
+
+		double l = 6.5;
+		double s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 	}
 }
